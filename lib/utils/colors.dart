29433790a08440e621bc6cad40e05e6741962494
salsabill4 <147946866+salsabill4@users.noreply.github.com>
2024-06-19 10:33:47 +0700

    Create colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors{
+  static final Color blackColor = const Color(0xFF000000);
+  static final Color whiteColor = const Color(0xFFFFFFFF);
+  static final Color grey1Color = const Color(0xFF808080);
+  static final Color pink1Color = const Color(0xFFF5957E);
+  static final Color pink2Color = const Color(0xFFFCCFA6);
+  static final Color pink3Color = const Color(0xFFF7E2E1);
+  static final Color brown1Color = const Color(0xFF9C553C);
+  static final Color brown2Color = const Color(0xFFC0937E);
+  static final Color blue1Color = const Color(0xFFADD4D1);
+  
+}
